442064bba0e4382fb76582aeaecb4ff228019f4f
nils <nils@bennerscheidt.de>
2024-10-05 09:32:07 +0200

    Image gallary

--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
 function rmHandle() {
     console.log("hi")
     return 1
-    
+
 }
 
-function menuToggle(){
+function menuToggle() {
     const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.classList.toggle('hidden'); 
+    mobileMenu.classList.toggle('hidden');
+}
+
+const images = document.querySelectorAll('#gallery img');
+let currentIndex = 0;
+
+function openModal(index) {
+    currentIndex = index;
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = images[currentIndex].src;
+    document.getElementById('imageModal').classList.remove('hidden');
+    updateButtons();
+}
+
+function closeModal() {
+    document.getElementById('imageModal').classList.add('hidden');
+}
+
+function prevImage() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = images[currentIndex].src;
+        updateButtons();
+    }
+}
+
+function nextImage() {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = images[currentIndex].src;
+        updateButtons();
+    }
+}
+
+function updateButtons() {
+    // Disable "Previous" button when at the first image
+    document.getElementById('prevBtn').disabled = currentIndex === 0;
+    // Disable "Next" button when at the last image
+    document.getElementById('nextBtn').disabled = currentIndex === images.length - 1;
 }
