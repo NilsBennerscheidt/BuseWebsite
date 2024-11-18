ea0c43098d26425ec015c9f8054656bbbc58b1a8
nils <nils@bennerscheidt.de>
2024-11-18 22:48:21 +0100

    Images

--- js/script.js
+++ js/script.js
@@ -130,3 +130,8 @@ document.addEventListener('keydown', function (event) {
 
 nextImage();
 getStormInfo();
+window.addEventListener('load', function() {
+if (window.location.hash) {
+    window.location.href = window.location.href.split('#')[0];
+}
+});
